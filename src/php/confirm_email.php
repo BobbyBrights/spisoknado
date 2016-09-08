2f70647cee1f17bb1aeac70a51465639f04fc89f
veruviisusa <r.gerasimovich@white-soft.ru>
2016-09-08 15:28:32 +1000

    правки

--- src/php/confirm_email.php
+++ src/php/confirm_email.php
@@ -2,22 +2,16 @@
   $email = $_POST['email'];
   $code = $_POST['code'];
 
-  $to  = $email;
+  $to = $email;
 
   $subject = "Активация аккаунта на spisoknado";
 
-  $message = '
-  <html>
-      <head>
-          <title>Активация аккаунта на spisoknado</title>
-      </head>
-      <body>
-          <p>Поздравляем, вы успешно зарегистрировались на сайте spisoknado. Для активации аккаунта перейдите по ссылке: <a href="http://romger.tmweb.ru/spisoknado/#/login?email=$email&code=$code">ссылка активация</a></p>
-      </body>
-  </html>';
+  $message = '<html><head><title>Активация аккаунта на spisoknado</title></head><body><p>Поздравляем, вы успешно зарегистрировались на сайте spisoknado. Для активации аккаунта перейдите по ссылке: <a href="http://romger.tmweb.ru/spisoknado/#/login?email='.$email.'&code='.$code.'">ссылка активация</a></p></body></html>';
 
   $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
   $headers .= "From: spisoknado\r\n";
 
   mail($to, $subject, $message, $headers);
+
+  echo $to.' '.$message;
 ?>
